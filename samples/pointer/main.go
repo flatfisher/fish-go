f92291e235bbfc883fe58918f6625feb5b6cab80
Kyohei Tsuda <shimano.entou@gmail.com>
2018-03-18 14:31:23 +0900

    Add sample

---
+++ samples/pointer/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Sample is sample.
+type Sample struct {
+	X int
+	Y int
+}
+
+func exchangeSample(s *Sample) {
+	x, y := s.Y, s.X
+	s.X = x
+	s.Y = y
+}
+
+func main() {
+	s := Sample{X: 1, Y: 2}
+	exchangeSample(&s)
+	fmt.Println(s.X)
+	fmt.Println(s.Y)
+}
