968ff5afb856182f7b3aa6d16f390334454b251d
flatfisher <shimano.entou@gmail.com>
2019-02-10 15:38:31 +0900

    Update sample

--- url-scrape/main.go
+++ url-scrape/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
 )
 
 func main() {
@@ -21,7 +27,31 @@ func main() {
 			if res.StatusCode == 200 {
 				fmt.Println(url)
 			}
+
+			z := html.NewTokenizer(res.Body)
+			content := []string{}
+			// While have not hit the </html> tag
+			for z.Token().Data != "html" {
+				tt := z.Next()
+				if tt == html.StartTagToken {
+					t := z.Token()
+					if t.Data == "td" {
+						inner := z.Next()
+						if inner == html.TextToken {
+							text, err := sJisttoUtf8((string)(z.Text()))
+							if err != nil {
+								log.Fatal(err)
+							}
+							t := strings.TrimSpace(text)
+							content = append(content, t)
+						}
+					}
+				}
+			}
+			// Print to check the slice's content
+			fmt.Println(content)
 		}
+		break
 	}
 }
 
@@ -30,4 +60,12 @@ func getString(n int) string {
 		return fmt.Sprintf("%d", n)
 	}
 	return fmt.Sprintf("0%d", n)
+}
+
+func sJisttoUtf8(str string) (string, error) {
+	ret, err := ioutil.ReadAll(transform.NewReader(strings.NewReader(str), japanese.ShiftJIS.NewDecoder()))
+	if err != nil {
+		return "", err
+	}
+	return string(ret), err
 }
