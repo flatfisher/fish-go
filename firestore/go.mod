7e1fd238e301543c13d9e563f1cec4403c8453b8
flatfisher <shimano.entou@gmail.com>
2019-02-18 22:02:40 +0900

    Update

---
+++ firestore/go.mod
@@ -0,0 +1,8 @@
+module firestore
+
+require (
+	cloud.google.com/go v0.26.0
+	firebase.google.com/go v3.6.0+incompatible
+	github.com/googleapis/gax-go v2.0.2+incompatible // indirect
+	google.golang.org/api v0.1.0
+)
---
+++ firestore/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"google.golang.org/api/iterator"
+
+	"cloud.google.com/go/firestore"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// [START fs_initialize]
+	// Sets your Google Cloud Platform project ID.
+	projectID := ""
+
+	// Get a Firestore client.
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	// Close client when done.
+	defer client.Close()
+	// [END fs_initialize]
+
+	// [START fs_add_data_1]
+	_, _, err = client.Collection("users").Add(ctx, map[string]interface{}{
+		"first": "Ada",
+		"last":  "Lovelace",
+		"born":  1815,
+	})
+	if err != nil {
+		log.Fatalf("Failed adding alovelace: %v", err)
+	}
+	// [END fs_add_data_1]
+
+	// [START fs_add_data_2]
+	_, _, err = client.Collection("users").Add(ctx, map[string]interface{}{
+		"first":  "Alan",
+		"middle": "Mathison",
+		"last":   "Turing",
+		"born":   1912,
+	})
+	if err != nil {
+		log.Fatalf("Failed adding aturing: %v", err)
+	}
+	// [END fs_add_data_2]
+
+	// [START fs_get_all_users]
+	iter := client.Collection("users").Documents(ctx)
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate: %v", err)
+		}
+		fmt.Println(doc.Data())
+	}
+	// [END fs_get_all_users]
+}
