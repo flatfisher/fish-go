fbd634d15d6f15687345581dd3cb71ab0e83d3f3
flatfisher <shimano.entou@gmail.com>
2019-02-23 13:07:43 +0900

    prepareRetrieve

---
+++ firestore-snippets/retrieve.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+
+	"cloud.google.com/go/firestore"
+)
+
+func prepareRetrieve(ctx context.Context, client *firestore.Client) error {
+	// [START fs_retrieve_create_examples]
+	cities := []struct {
+		id string
+		c  City
+	}{
+		{id: "SF", c: City{Name: "San Francisco", State: "CA", Country: "USA", Capital: false, Population: 860000}},
+		{id: "LA", c: City{Name: "Los Angeles", State: "CA", Country: "USA", Capital: false, Population: 3900000}},
+		{id: "DC", c: City{Name: "Washington D.C.", Country: "USA", Capital: true, Population: 680000}},
+		{id: "TOK", c: City{Name: "Tokyo", Country: "Japan", Capital: true, Population: 9000000}},
+		{id: "BJ", c: City{Name: "Beijing", Country: "China", Capital: true, Population: 21500000}},
+	}
+	for _, c := range cities {
+		_, err := client.Collection("cities").Doc(c.id).Set(ctx, c.c)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
