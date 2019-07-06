3ac25acab9081b1cba7406bb8d4fe0817606ba11
flatfisher <shimano.entou@gmail.com>
2019-07-06 16:21:10 +0900

    colorstring

---
+++ colorstring/go.mod
@@ -0,0 +1,5 @@
+module colorstring
+
+go 1.12
+
+require github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db
---
+++ colorstring/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/mitchellh/colorstring"
+)
+
+func main() {
+	colorstring.Println("[blue]Hello [red]World!")
+}
