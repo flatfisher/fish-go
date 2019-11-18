7745b35b0f74d4ee75dc108e130ffb8919b11477
flatfisher <shimano.entou@gmail.com>
2019-11-18 19:24:18 +0900

    Update

--- gomock/go.mod
+++ gomock/go.mod
@@ -2,4 +2,4 @@ module gomock
 
 go 1.13
 
-require github.com/golang/mock v1.3.1 // indirect
+require github.com/golang/mock v1.3.1
--- gomock/user_test.go
+++ gomock/user_test.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"testing"
+	"github.com/golang/mock/gomock"
+)
+
+func TestUser_GetName(t *testing.T){
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	m := mock_main.NewMockUser(ctrl)
+}
